ffb452f59f96f5d8750fdfdd31f0202e71ada1da
doosoon choi <doosoon@gmail.com>
2016-08-14 15:36:21 +0900

    Add colors and change text font size and alignment

--- app/src/main/java/com/doosoon/home/bluema/CityListItemView.java
+++ app/src/main/java/com/doosoon/home/bluema/CityListItemView.java
@@ -1,7 +1,9 @@
 package com.doosoon.home.bluema;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,6 +22,16 @@ class CityListItemView extends LinearLayout {
         cityName.setText(data.name);
         TextView cityPrice = (TextView) findViewById(R.id.cityPrice);
         cityPrice.setText(String.format("%d", data.price));
+        ImageView colorBar = (ImageView) findViewById(R.id.color);
+        if(data.color == 0)
+            colorBar.setBackgroundResource(R.color.red);
+        else if(data.color == 1)
+            colorBar.setBackgroundResource(R.color.green);
+        else if(data.color == 2)
+            colorBar.setBackgroundResource(R.color.brown);
+        else
+            colorBar.setBackgroundResource(R.color.black);
+
         if (data.hotelPrice != 0) {
             TextView hotelPrice = (TextView) findViewById(R.id.hotelPrice);
             hotelPrice.setText(String.format("%d", data.price + data.hotelPrice));
--- app/src/main/java/com/doosoon/home/bluema/DataManager.java
+++ app/src/main/java/com/doosoon/home/bluema/DataManager.java
@@ -10,12 +10,13 @@ import java.util.List;
 public class DataManager {
 
     public static class CityData {
-        CityData(String name, int price, int hotel, int building, int villa) {
+        CityData(String name, int price, int hotel, int building, int villa, int color) {
             this.name = name;
             this.price = price;
             this.hotelPrice = hotel;
             this.buildingPrice = building;
             this.villaPrice = villa;
+            this.color = color;
         }
 
         String name;
@@ -23,38 +24,39 @@ public class DataManager {
         int hotelPrice;
         int buildingPrice;
         int villaPrice;
+        int color;
     }
 
     private static final List<CityData> mArrayCityData = Arrays.asList(
-            new CityData("타이베이", 5, 25, 15, 5),
-            new CityData("홍콩", 8, 25, 15, 5),
-            new CityData("마닐라", 8, 25, 15, 5),
-            new CityData("제주도", 20, 0, 0, 0),
-            new CityData("싱가포르", 10, 25, 15, 5),
-            new CityData("카이로", 10, 25, 15, 5),
-            new CityData("이스탄불", 12, 25, 15, 5),
-            new CityData("아테네", 14, 50, 30, 10),
-            new CityData("코펜하겐", 16, 50, 30, 10),
-            new CityData("스톡홀롬", 16, 50, 30, 10),
-            new CityData("콩코드 여객기", 20, 0, 0, 0),
-            new CityData("취리히", 18, 50, 30, 10),
-            new CityData("베를린", 18, 50, 30, 10),
-            new CityData("몬트리올", 20, 50, 30, 10),
-            new CityData("부에노스 아이레스", 22, 75, 45, 15),
-            new CityData("상파울루", 24, 75, 45, 15),
-            new CityData("시드니", 24, 75, 45, 15),
-            new CityData("부산", 50, 0, 0, 0),
-            new CityData("하와이", 26, 75, 45, 15),
-            new CityData("리스본", 26, 75, 45, 15),
-            new CityData("퀸 에리자베스호", 30, 0, 0, 0),
-            new CityData("마드리드", 28, 75, 45, 15),
-            new CityData("도쿄", 30, 100, 60, 20),
-            new CityData("콜럼비아호", 45, 0, 0, 0),
-            new CityData("파리", 32, 100, 60, 20),
-            new CityData("로마", 32, 100, 60, 20),
-            new CityData("런던", 35, 100, 60, 20),
-            new CityData("뉴욕", 35, 100, 60, 20),
-            new CityData("서울올림픽", 100, 0, 0, 0));
+            new CityData("타이베이", 5, 25, 15, 5, 0),
+            new CityData("홍콩", 8, 25, 15, 5, 0),
+            new CityData("마닐라", 8, 25, 15, 5, 0),
+            new CityData("제주도", 20, 0, 0, 0, 0),
+            new CityData("싱가포르", 10, 25, 15, 5, 0),
+            new CityData("카이로", 10, 25, 15, 5, 0),
+            new CityData("이스탄불", 12, 25, 15, 5, 0),
+            new CityData("아테네", 14, 50, 30, 10, 1),
+            new CityData("코펜하겐", 16, 50, 30, 10, 1),
+            new CityData("스톡홀롬", 16, 50, 30, 10, 1),
+            new CityData("콩코드 여객기", 20, 0, 0, 0, 1),
+            new CityData("취리히", 18, 50, 30, 10, 1),
+            new CityData("베를린", 18, 50, 30, 10, 1),
+            new CityData("몬트리올", 20, 50, 30, 10, 1),
+            new CityData("부에노스 아이레스", 22, 75, 45, 15, 2),
+            new CityData("상파울루", 24, 75, 45, 15, 2),
+            new CityData("시드니", 24, 75, 45, 15, 2),
+            new CityData("부산", 50, 0, 0, 0, 2),
+            new CityData("하와이", 26, 75, 45, 15, 2),
+            new CityData("리스본", 26, 75, 45, 15, 2),
+            new CityData("퀸 에리자베스호", 30, 0, 0, 0, 2),
+            new CityData("마드리드", 28, 75, 45, 15, 2),
+            new CityData("도쿄", 30, 100, 60, 20, 3),
+            new CityData("콜럼비아호", 45, 0, 0, 0, 3),
+            new CityData("파리", 32, 100, 60, 20, 3),
+            new CityData("로마", 32, 100, 60, 20, 3),
+            new CityData("런던", 35, 100, 60, 20, 3),
+            new CityData("뉴욕", 35, 100, 60, 20, 3),
+            new CityData("서울올림픽", 100, 0, 0, 0, 3));
 
     public DataManager() {
 
